de703ea2b51d5de0d9d55a01ed7b7e55a8dc751a
Andres Moreno <aymorenov@eafit.edu.co>
2024-02-24 19:58:53 -0500

    heavily modified eight_channel_relay.ino file

--- src/Battery-Swapping/modules_controls/eight_channel_relay/eight_channel_relay.ino
+++ src/Battery-Swapping/modules_controls/eight_channel_relay/eight_channel_relay.ino
@@ -1,79 +1,133 @@
-
-#include <SPI.h>
 #include <mcp2515.h> 
-#include <ArduinoCanUtils.h>
-#include <Adafruit_NeoPixel.h>
+#include <CanUtils.h>
 
-
-// CAN-BUS SET-UP
+// DEFINITION OF CAN-BUS VARIABLES 
 struct can_frame canMsg; 
-const int GLOBAL_CAN_CS_PIN = 9;
-MCP2515 mcp2515Global(GLOBAL_CAN_CS_PIN); 
+MCP2515 canNetworkGlobal(9); // CS pin of global CAN network is 9.
 
-enum statusOfCanMsg {FAIL, OK}
-statusOfCanMsg readGlobalNetwork(struct can_frame &canMsg)
-{
-  statusOfCanMsg msgStatus;
-  if (mcp2515Global.readMessage(&canMsg) == MCP2515::ERROR_OK) {
-    msgStatus = OK;
-  }else{
-    msgStatus = FAIL;
-  }
-  return msgStatus;
-};
-
-
-
-
-
+// DEFINITION OF EIGHT-CHANNEL RELAY MODULE CLASS 
 class EightChannelRelayModule
 {
   public:
-  uint8_t originAddress;
-  enum stateOfChannel {OFF, ON};
+  enum CHANNEL_STATE {OFF, ON};
+  enum CHANNEL_NAME 
+  {
+    CHANNEL0,
+    CHANNEL1,
+    CHANNEL2,
+    CHANNEL3,
+    CHANNEL4,
+    CHANNEL5,
+    CHANNEL6,
+    CHANNEL7,    
+  };
+  canUtils::MODULE_ADDRESS moduleAddress = canUtils::EIGHT_CHANNEL_RELAY;
+
+  CHANNEL_STATE channelStates[8];
   const int CHANNEL_PINS[8] = {A0, A1, A2, A3, A4, A5, 4, 5};
-  stateOfChannel channelStates[8] = {OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF};
+  
+  void EightChannelRelayModule(){}
 
-
-  void EightChannelRelayModule(uint8_t moduleOriginAddress)
-  {
-  originAddress = moduleOriginAddress;
+  void stdSetUp(){
+    for(int i; i<8; i++){pinMode(CHANNEL_PINS[i], OUTPUT);}
+    setChannelStates({OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF});
   }
-  void setChannelState(uint8_t idx, stateOfChannel state)
-  {
-      if(idx<8 && (state==OFF || state==ON)){
-          channelStates[idx] = state;
-          digitalWrite(CHANNEL_PINS[idx], state);
+  void net2rpy_sendChannelStates(MCP2515& canNetwork, struct can_frame* p_canMsg){
+    p_canMsg->can_dlc = 8;
+    for (int i=0; i<8; i++){p_canMsg->data[i] = channelStates[i];}
+
+    p_canMsg->can_id = 
+    canUtils::createCanMsgCanId(canUtils::MEDIUM_LOW, 
+                                canUtils::STATES_OF_EIGHT_CHANNEL_RELAY_MODULE, 
+                                canUtils::CONTROL_CENTER,
+                                moduleAddress);
+
+    canNetwork.sendMessage(p_canMsg)
+  }
+
+
+  void rpy2net_readAndExecuteCommands(MCP2515& canNetwork, struct can_frame* p_canMsg){
+    if (canUtils::readCanMsg(canNetwork, p_canMsg, moduleAddress, canUtils::CONTROL_CENTER) == MCP2515::ERROR_OK){
+
+      canUtils::ACTIVITY_CODE activityCode = 
+      canUtils::getActivityCodeFromCanMsgCanId(p_canMsg->can_id);
+
+      switch (activityCode) {
+        case canUtils::rpy2net_SET_STATE_OF_EIGHT_CHANNEL_RELAY_MODULE:
+          rpy2net_setChannelState(p_canMsg->data);
+          break;
+        case canUtils::rpy2net_SET_STATES_OF_EIGHT_CHANNEL_RELAY_MODULE:
+          rpy2net_setChannelStates(p_canMsg->data);
+          break;
+        case canUtils::rpy2net_FLIP_STATE_OF_EIGHT_CHANNEL_RELAY_MODULE:
+          rpy2net_fipChannelState(p_canMsg->data);
+          break;
+        case canUtils::rpy2net_FLIP_STATES_OF_EIGHT_CHANNEL_RELAY_MODULE:
+          rpy2net_fipChannelStates(p_canMsg->data);
+          break;
       }
-  }
-  void fipChannelState(uint8_t idx)
-  {
-    setChannelState(idx, !channelStates[idx]);
+    }
   }
 
+  void setChannelState(CHANNEL_NAME name, CHANNEL_STATE state){
+        digitalWrite(CHANNEL_PINS[name], state);
+        channelStates[name] = state;    
+  }
+  void setChannelStates(CHANNEL_STATE states[8]){
+    for (int i=0; i<8; i++){
+      setChannelState(static_cast<CHANNEL_NAME>(i), states[i]);
+    }
+  }
+  void fipChannelState(CHANNEL_NAME name){
+    setChannelState(name, !channelStates[name]);
+  }
+  void fipChannelStates(bool flipLogic[8]){
+    for (int i=0; i<8; i++){
+      if (flipLogic[i]){
+        fipChannelState(static_cast<CHANNEL_NAME>(i));
+      }
+    }
+  }
 
-
+  void rpy2net_setChannelState(uint8_t canData[8]){
+    CHANNEL_NAME name = static_cast<CHANNEL_NAME>(canData[0]);
+    CHANNEL_STATE state = static_cast<CHANNEL_STATE>(canData[1]);
+    setChannelState(name, state);
+  }
+  void rpy2net_setChannelStates(uint8_t canData[8]){
+    CHANNEL_STATE states[8];
+    for (int i=0; i<8; i++){states[i] = static_cast<CHANNEL_STATE>(canData[i]);}
+    setChannelStates(states);
+  }
+  void rpy2net_fipChannelState(uint8_t canData[8]){
+    CHANNEL_NAME name = static_cast<CHANNEL_NAME>(canData[0]);
+    fipChannelState(name);
+  }
+  void rpy2net_fipChannelStates(uint8_t canData[8]){
+    bool flipLogic[8];
+    for (int i=0; i<8; i++){flipLogic[i] = static_cast<bool>(canData[i]);}
+    fipChannelStates(flipLogic);
+  }
 };
 
 
-EightChannelRelayModule eightChannelRelayModule(1);
-
-
+EightChannelRelayModule eightChannelRelayModule;
 
 
 void setup(){
   
-  for(int i; i<8; i++){
-    pinMode(EIGHT_CHANNEL_RELAY_CHANNEL_PINS[i], OUTPUT);
-    digitalWrite(EIGHT_CHANNEL_RELAY_CHANNEL_PINS[i], eight_channel_relay_module_states[i]);
-    }
+  // Eight-channel relay module standard set-up.
+  eightChannelRelayModule.stdSetUp();
 
-  mcp2515Global.reset();
-  mcp2515Global.setBitrate(CAN_250KBPS, MCP_8MHZ); // The MCP2515 arduino module, has  an 8MHz clock.
-  mcp2515Global.setNormalMode();
+  // CAN Network standard set-up.
+  canUtils::stdCanNetworkSetUp(canNetworkGlobal)
+
 }
 
 void loop(){
-  
-  
+
+  eightChannelRelayModule.net2rpy_sendChannelStates(canNetworkGlobal, &canMsg);
+  eightChannelRelayModule.rpy2net_readAndExecuteCommands(canNetworkGlobal, &canMsg);
+
 }
+
