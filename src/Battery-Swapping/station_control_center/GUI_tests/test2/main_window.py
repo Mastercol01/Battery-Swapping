2cd3b78ac9fea1b1e97d03f80337559d20351576
Andrés Moreno <aymorenov@eafit.edu.co>
2024-03-04 11:10:08 -0500

    Serial read

--- src/Battery-Swapping/station_control_center/GUI_tests/test2/main_window.py
+++ src/Battery-Swapping/station_control_center/GUI_tests/test2/main_window.py
@@ -1,36 +1,43 @@
 import time
 import serial
-import logging
+from functools import partial
 from PyQt5.QtWidgets import qApp, QMainWindow
-from PyQt5.QtCore import QObject, QRunnable, QThreadPool, pyqtSignal, pyqtSlot
+from PyQt5.QtCore import QObject, QRunnable, QThreadPool, pyqtSignal, pyqtSlot, QTimer
 
 
 #%%                   DEFINITION OF WORKER CLASS FOR READING SERIAL DATA
 
 class WorkerSignals(QObject):
     results = pyqtSignal(str)
+    reading_state = pyqtSignal(bool)
+
 
 
 class SerialReadWorker(QRunnable):
     def __init__(self, ser):
         super(SerialReadWorker, self).__init__()
         self.ser = ser
+        self.keep_reading = True
         self.signals = WorkerSignals()
+        return None
+    
+    def setReadingState(self, state):
+        self.keep_reading = state
         return None
 
     @pyqtSlot()
     def run(self):
-        while True:
+        while self.keep_reading:
             if self.ser.in_waiting > 0:
                 line = self.ser.readline().decode("utf-8").rstrip()
-                #self.signals.results.emit(line)
-                print(line)
+                self.signals.results.emit(line)
         return None
 
 
 #%%              DEFINITION OF MAIN WINDOW WIDGET
 
 class MainWindow(QMainWindow):
+    signals = WorkerSignals()
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setup()
@@ -52,12 +59,23 @@ class MainWindow(QMainWindow):
             qApp.exit()
 
         worker = SerialReadWorker(self.ser)
-        #worker.signals.results.connect(self.serialPrint)
+        worker.signals.results.connect(self.serialPrint)
+        self.signals.reading_state.connect(worker.setReadingState)
         self.threadpool.start(worker)
+
+        QTimer.singleShot(8000,  partial(self.setReadingState, False))
         return None
     
-    def serialPrint(line):
+    def serialPrint(self, line):
         print(line)
+        return None
+    
+    def setReadingState(self, state):
+        self.signals.reading_state.emit(state)
+        return None
+    
+    def keepSerialRead(self):
+        self.signals.keep_reading.emit()
         return None
     
     def closeEvent(self, event):
