30d78a9e0685d163bda9f2a87182292b1ce9821e
Andrés Moreno <aymorenov@eafit.edu.co>
2024-03-11 18:40:46 -0500

    added test for GUI windows

--- src/Battery-Swapping/station_control_center/GUI/v3/BSS_control/control_center.py
+++ src/Battery-Swapping/station_control_center/GUI/v3/BSS_control/control_center.py
@@ -49,8 +49,8 @@ class ControlCenter:
         self.modules[self.EIGHT_CHANNEL_RELAY_ADDRESS].SIGNALS.sendCanMsg_eightChannelRelay.connect(sendCanMsg_func)
         return None
     
-def getSlotsThatMatchStates(self, statesToMatch):
-        statesToMatchLogic = [
+    def getSlotsThatMatchStates(self, statesToMatch):
+        statesToMatchLogic = {
             "LIMIT_SWITCH"                     : (True,  "limitSwitchState"),
             "LED_STRIP"                        : (True,  "ledStripState"), 
             "BMS"                              : (True,  "bmsSolenoidState"),
@@ -68,11 +68,11 @@ def getSlotsThatMatchStates(self, statesToMatch):
             "BATTERY_IS_CHARGED"               : (False, "isCharged"),
             "BATTERY_IS_CHARGABLE"             : (False, "isChargable"),
             "BATTERY_HAS_WARNINGS"             : (False, "hasWarnings"),
-            "BATTERY_HAS_FATAL_WARNINGS"       : (False, "isDamaged"),
-            "BATTERY_IS_DAMAGED"               : (False, "hasFatalWarnings"),
-            "BATTERY_IS_DELIVERABLE_TO_USER"   : (False, "hasFatalWarnings")
-        ]
-        statesToMatch_ = {key:value for key,value in statesToMatch.items() if key in ALLOWED_KEYS}
+            "BATTERY_HAS_FATAL_WARNINGS"       : (False, "hasFatalWarnings"),
+            "BATTERY_IS_DAMAGED"               : (False, "isDamaged"),
+            "BATTERY_IS_DELIVERABLE_TO_USER"   : (False, "isDeliverableToUser")
+        }
+        statesToMatch_ = {key:value for key,value in statesToMatch.items() if key in statesToMatchLogic.keys()}
         res = [slotAddress for slotAddress in self.SLOT_ADDRESSES]
 
         for stateKeyword, stateValueToMatch in statesToMatch_.items():
@@ -80,136 +80,25 @@ def getSlotsThatMatchStates(self, statesToMatch):
             if len(res) < 1:
                 break
 
-            if stateKeyword in ["LIMIT_SWITCH", "BATTERY_IN_SLOT"]:
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].limitSwitchState == stateValueToMatch]
+            isSlotAttr, attrName = statesToMatchLogic[stateKeyword]
 
-            elif stateKeyword == "LED_STRIP":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].ledStripState == stateValueToMatch]
-
-            elif stateKeyword in ["BMS", "BATTERY_BMS_IS_ON"]:
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].solenoidsStates[SOLENOID_NAME.BMS.value] == stateValueToMatch]
-
-            elif stateKeyword == "DOOR_LOCK":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].solenoidsStates[SOLENOID_NAME.DOOR_LOCK.value] == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_LOCK":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].solenoidsStates[SOLENOID_NAME.BATTERY_LOCK.value] == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_IS_WAITING_FOR_ALL_DATA":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.waitingForAllData == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_BMS_HAS_CAN_BUS_ERROR":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.bmsHasCanBusError == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_IS_ADDRESSABLE":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.isAddressable == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_IS_CHARGING":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.isCharging == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_IS_CHARGED":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.isCharged == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_IS_CHARGABLE":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.isChargable == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_HAS_WARNINGS":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.hasWarnings == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_HAS_FATAL_WARNINGS":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.hasFatalWarnings == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_IS_DAMAGED":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.isDamaged == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_IS_DELIVERABLE_TO_USER":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.isDeliverableToUser == stateValueToMatch]
+            if isSlotAttr:
+                res = [slotAddress for slotAddress in res if getattr(self.modules[slotAddress], attrName) == stateValueToMatch]
+            else:
+                res = [slotAddress for slotAddress in res if getattr(self.modules[slotAddress].battery, attrName) == stateValueToMatch]
 
         return res
 
-    def getSlotsThatMatchStates(self, statesToMatch):
-        ALLOWED_KEYS = [
-            "LIMIT_SWITCH"                     ,
-            "LED_STRIP"                        ,
-            "BMS"                              ,
-            "DOOR_LOCK"                        ,
-            "BATTERY_LOCK"                     ,
-
-            "BATTERY_IN_SLOT"                  ,
-            "BATTERY_BMS_IS_ON"                ,
-            "BATTERY_IS_WAITING_FOR_ALL_DATA"  ,
-            "BATTERY_BMS_HAS_CAN_BUS_ERROR"    ,
-
-            "BATTERY_IS_ADDRESSABLE"           ,
-            "BATTERY_IS_CHARGING"              ,
-            "BATTERY_IS_CHARGED"               ,
-            "BATTERY_IS_CHARGABLE"             ,
-            "BATTERY_HAS_WARNINGS"             ,
-            "BATTERY_HAS_FATAL_WARNINGS"       ,
-            "BATTERY_IS_DAMAGED"               ,
-            "BATTERY_IS_DELIVERABLE_TO_USER"   
-        ]
-        statesToMatch_ = {key:value for key,value in statesToMatch.items() if key in ALLOWED_KEYS}
-        res = [slotAddress for slotAddress in self.SLOT_ADDRESSES]
-
-        for stateKeyword, stateValueToMatch in statesToMatch_.items():
-
-            if len(res) < 1:
-                break
-
-            if stateKeyword in ["LIMIT_SWITCH", "BATTERY_IN_SLOT"]:
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].limitSwitchState == stateValueToMatch]
-
-            elif stateKeyword == "LED_STRIP":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].ledStripState == stateValueToMatch]
-
-            elif stateKeyword in ["BMS", "BATTERY_BMS_IS_ON"]:
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].solenoidsStates[SOLENOID_NAME.BMS.value] == stateValueToMatch]
-
-            elif stateKeyword == "DOOR_LOCK":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].solenoidsStates[SOLENOID_NAME.DOOR_LOCK.value] == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_LOCK":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].solenoidsStates[SOLENOID_NAME.BATTERY_LOCK.value] == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_IS_WAITING_FOR_ALL_DATA":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.waitingForAllData == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_BMS_HAS_CAN_BUS_ERROR":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.bmsHasCanBusError == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_IS_ADDRESSABLE":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.isAddressable == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_IS_CHARGING":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.isCharging == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_IS_CHARGED":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.isCharged == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_IS_CHARGABLE":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.isChargable == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_HAS_WARNINGS":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.hasWarnings == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_HAS_FATAL_WARNINGS":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.hasFatalWarnings == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_IS_DAMAGED":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.isDamaged == stateValueToMatch]
-
-            elif stateKeyword == "BATTERY_IS_DELIVERABLE_TO_USER":
-                res = [slotAddress for slotAddress in res if self.modules[slotAddress].battery.isDeliverableToUser == stateValueToMatch]
-
-        return res
+    
     
 
     def turnOnLedStripsBasedOnState(self):
         batteries_notInSlot = self.getSlotsThatMatchStates({"BATTERY_IN_SLOT": False})
         batteries_damaged = self.getSlotsThatMatchStates({"BATTERY_IS_DAMAGED": True})
         batteries_deliverableToUser = self.getSlotsThatMatchStates({"BATTERY_IS_DELIVERABLE_TO_USER": True})
-        other_batteries = self.getSlotsThatMatchStates({"BATTERY_IN_SLOT": True, "BATTERY_IS_DELIVERABLE_TO_USER":False, "BATTERY_IS_DAMAGED": False})
+        other_batteries = self.getSlotsThatMatchStates({"BATTERY_IN_SLOT": True,
+                                                        "BATTERY_IS_DELIVERABLE_TO_USER":False, 
+                                                        "BATTERY_IS_DAMAGED": False})
 
         print(batteries_notInSlot)
         print(batteries_damaged)
--- src/Battery-Swapping/station_control_center/GUI/v3/main_window.py
+++ src/Battery-Swapping/station_control_center/GUI/v3/main_window.py
@@ -30,7 +30,7 @@ from PyQt5.QtWidgets import (
     QStyle,
     QStackedWidget)
 
-class WINDOWS(Enum):
+class WINS(Enum):
     INFO_WINDOW = 0
     LOCK_SCREEN = 1
     OPTIONS_PANEL = 2
@@ -39,24 +39,56 @@ class WINDOWS(Enum):
 
 
 class MainWindow(QMainWindow):
+
     def __init__(self):
         super().__init__()
+        self.globalTimers_setup()
+        self.windows_setup()
 
         return None
     
-    def globalTimer_setup(self):
+    def globalTimers_setup(self):
         self.currentGlobalTime = 0
-        self.globalTimer = QTimer()
-        self.globalTimer.setInterval(250)
-        self.globalTimer.timeout.connect(self.updateTimeDependentVars)
-        self.globalTimer.start()
+
+        self.globalTimers = {
+              250 : QTimer(),
+            60000 : QTimer() 
+        }
+        for key in self.globalTimers.keys():
+            self.globalTimers[key].setInterval(key)
+
+        self.globalTimers[250].timeout.connect(self.updateGlobalTimerVars250)
+        self.globalTimers[60000].timeout.connect(self.updateGlobalTimerVars60000)
+
+        for key in self.globalTimers.keys():
+            self.globalTimers[key].setInterval(key)
         return None
-    def updateTimeDependentVars(self):
+    def updateGlobalTimerVars250(self):
         self.currentGlobalTime += 0.25
         return None
+    def updateGlobalTimerVars60000(self):
+        self.windows[WINS.LOCK_SCREEN].dateClock.updateTime()
+        self.windows[WINS.LOCK_SCREEN].dateClock.updateDate()
+        return None
+        
+
+
+        
 
     def windows_setup(self):
-        self.windows = QStackedWidget()
+        self.stckdWidget = QStackedWidget()
 
+        self.windows = {
+            WINS.INFO_WINDOW       : InfoWindow(),
+            WINS.LOCK_SCREEN       : LockScreenWindow(),
+            WINS.OPTIONS_PANEL     : OptionsPanelWindow(),
+            WINS.USER_PROMPT_PANEL : UserPromptPanelWindow()
+        }
+        
+        for key in self.windows.keys():
+            self.stckdWidget.insertWidget(key.value, self.windows[key])
 
+        self.stckdWidget.setCurrentIndex(WINS.OPTIONS_PANEL.value)
+
+        self.setCentralWidget(self.stckdWidget)
         return None
